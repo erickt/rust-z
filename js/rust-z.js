27417e8bef1e77c4e4c66728ccab5ee50d960805
Brian Anderson <banderson@mozilla.com>
2016-07-12 20:16:29 -0700

    Initial

---
+++ js/rust-z.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+    // Configure the page for a particular scene based on the URL
+    moveToScene(parseUrl(document.URL));
+
+    // When the `#` in the page URL changes, change the scene again!
+    $(window).on('hashchange', function() {
+	moveToScene(parseUrl(document.URL));
+    });
+
+    // And yup, navigate to #scene whene a scene button is clicked
+    $("#section-controls > .scene").click(function() {
+	if (this.id == "scene-overview") {
+	    // The 'overview' button goes to the pretties URL.
+	    // #overview would work as well, and not require a reload.
+	    window.location = "./";
+	} else {
+	    window.location = "#" + this.innerHTML;
+	}
+    });
+
+    // Show the bottom half of the battle
+    $("#toggle-top").click(function() {
+	$("#options").toggleClass("option-top");
+    });
+});
+
+function parseUrl(url) {
+    var scene = "overview";
+    var filter = "";
+    var select = "";
+    if (url.indexOf("#") == -1) {
+	// pass
+    } else {
+	var tail = url.substring(url.indexOf("#") + 1);
+	var anchor = "";
+	if (tail.indexOf("?") == -1) {
+	    anchor = tail;
+	} else {
+	    anchor = tail.substring(0, tail.indexOf("?"));
+	    var query = tail.substring(tail.indexOf("?") + 1);
+	    var params = query.split("&");
+	    for (param of params) {
+		var parts = param.split("=");
+		if (parts.length == 2) {
+		    if (parts[0].startsWith("filter-")) {
+			filter = parts[0] + "-" + parts[1];
+		    }
+		    if (parts[0].startsWith("select-")) {
+			select = parts[0] + "-" + parts[1];
+		    }
+		}
+	    }
+	}
+
+	if (anchor == "") {
+	    // pass
+	} else {
+	    scene = anchor;
+	}
+    }
+
+    return {
+	scene: scene,
+	filter: filter,
+	select: select,
+    };
+}
+
+function moveToScene(scene) {
+    $("div#scene").removeClass();
+    $("div#scene").addClass(scene.scene);
+    $("div#filter").removeClass();
+    $("div#filter").addClass(scene.filter);
+    $("div#select").removeClass();
+    $("div#select").addClass(scene.select);
+    $("div#section-controls .scene").removeClass("selected");
+    $("div#section-controls .scene").
+	filter(function(i, el) { return el.innerHTML == scene.scene; }).
+	addClass("selected");
+}
---
+++ notes.md
@@ -0,0 +1,52 @@
+# goals
+
+- give users and contributors a picture of what's happening in rust and where it's going
+- inspire contributors and users with a common vision
+- progressive detail - on first glance anybody gets a high-level overview of the project, but you can drill down further to find details of individual work items
+- link relevant conversations, issues, rfcs
+- provide entry points for contributors
+- link to tracking issues for initatives
+- illuminate current initiatives
+- illuminate the RFC / feature pipeline
+- foreshadow future initiatives / "on-the-radar" topics
+- show progress towards goals
+- show what's in the next release
+- living updates - allow multiple people to maintain it, keep it updated as the project evolves and the zeitgeist changes
+
+# overview
+
+- battlfronts
+- battlfront top campaigns
+
+# sources
+
+- feature pipeline: RFCs, tracking issues, feature names
+- initatives: tracking issues, milestones
+- "on-the-radar" subjects: irlo, RFCs, issues
+- release notes
+- twir has FCP notices
+
+# features
+
+- rosters
+- point of contact
+- contributors
+- hotness
+- backburned campaigns
+- call out features on their own
+
+# long-term issues
+
+- linker-independence
+- HKT
+
+# long term ideas
+
+- gdb support
+- lldb support
+- bridges
+  - ruby,
+- rustdoc.org
+  - crates.fyi
+- homu-written-in-rust
+- crater-rewrite
